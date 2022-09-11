25751e9b1a29f7020555e655b615e73a5d95d28d
misaki12dev <misaki.12k@gmail.com>
2022-09-11 15:48:46 +0900

    generates random numbers and push to array

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import { render } from "@testing-library/react";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Dice from "./components/Dice";
 
-/**
- * Challenge:
- *
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
-
-
 export default function App() {
+  function allNewDice() {
+    // 1) empty array
+    const diceArray = [];
+    // loop up to 10
+    for (let i = 0; i < 10; i++) {
+      // create random num between 1 to 6
+      diceArray.push(Math.floor(Math.random() * 6) + 1)
+    }
+    return diceArray
+  }
 
-  return(
+  return (
     <main>
       <div className="container">
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
-        <Dice value={1}/>
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
+        <Dice value={1} />
       </div>
     </main>
-  )
+  );
 }
