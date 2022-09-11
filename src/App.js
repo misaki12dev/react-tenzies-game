f447c77ace36ed2e898bc8981974bec99210aa71
misaki12dev <misaki.12k@gmail.com>
2022-09-11 16:00:25 +0900

    map over dice elements

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import { render } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 import Dice from "./components/Dice";
 
 export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+
   function allNewDice() {
     // 1) empty array
     const diceArray = [];
-    // loop up to 10
+    // 2) loop up to 10
     for (let i = 0; i < 10; i++) {
-      // create random num between 1 to 6
-      diceArray.push(Math.floor(Math.random() * 6) + 1)
+      // 3) create random num between 1 to 6
+      diceArray.push(Math.floor(Math.random() * 6) + 1);
     }
-    return diceArray
+    return diceArray;
   }
+
+  // map elem and pass props to Dice
+  const diceElement = dice.map((num) => {
+    return <Dice value={num} />;
+  });
 
   return (
     <main>
-      <div className="container">
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-      </div>
+      <div className="container">{diceElement}</div>
     </main>
   );
 }
